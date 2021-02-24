05da734bdba415a88e33edf643d95cd1a01a7240
Erick Martinez <ermartinez13@gmail.com>
2021-02-24 17:55:25 -0600

    Add startGame and getNewQuestion implementations
    Along with setup of top-level variables and event listeners

---
+++ game.js
@@ -0,0 +1,48 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+const correctBonus = 10;
+const maxQuestions = 3;
+
+function startGame() {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [];
+  getNewQuestion();
+}
+
+function getNewQuestion() {
+  if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  availableQuestions.splice(questionIndex, 1);
+  question.innerText = currentQuestion;
+
+  choices.map((choice) => {
+    const choiceNumber = choice.dataset.number;
+    choice.innerText = currentQuestion[`choice` + choiceNumber];
+  });
+
+  acceptingAnswers = true;
+}
+
+choices.map((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+
+    const selectedAnswerNumber = e.target.dataset.number;
+  });
+});
+
+console.log(score);
